835df1acf7a50d34ec075af85ce4ad4306f165eb
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-11-25 11:05:24 +0530

    Add files via upload

---
+++ natural.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class natural
+{
+    public static void main(String arv[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        int sum=0;
+        for(int i=1;i<=s;i++)
+        {
+            sum+=i;
+        }
+        System.out.print(sum);
+    }
+}
+
+
