a81db4d9d5b260e887f681073074fc3d7c65c6d6
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-11-29 21:30:48 +0530

    Add files via upload

---
+++ addoftwono.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class addoftwono
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        try
+        {
+        int n=sc.nextInt();
+        int n1=sc.nextInt();
+        System.out.print(n+n1);
+        }
+        catch(Exception e)
+        {
+            System.out.print("Input error");
+        }
+    }
+}
